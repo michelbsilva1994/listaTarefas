0585865e915e293eb9866ff1bc5da982dc9818ab
michelbsilva1994 <michelbsilva1994@gmail.com>
2025-03-03 22:25:44 -0300

    continuação

---
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, ViewChild, inject } from '@angular/core';
+
+//Interface
+import { IListItems } from '../../interface/IListItems.interface';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
+
+  @ViewChild("inputText") public inputText!: ElementRef;
+
+  @Input({required: true}) public inputListItems:  IListItems[] = [];
+
+  @Output() public outputAddListItems = new EventEmitter<IListItems>();
+
+  public focusAndAddItem(value: string){
+    if(value){
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = '';
+
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
+      const id = `ID ${timestamp}`;
+
+      this.outputAddListItems.emit({
+        id,
+        checked: false,
+        value
+      });
+
+      return this.inputText.nativeElement.focus();
+    }
+  }
+}
